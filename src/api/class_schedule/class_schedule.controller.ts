8ba02146d9eb133d299fbd57c3f351d5ecf99d1c
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-08-30 16:29:23 +0700

    Update schedule route

--- src/api/class_schedule/class_schedule.controller.ts
+++ src/api/class_schedule/class_schedule.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Req, UseGuards, Get } from '@nestjs/common';
 import { ClassScheduleService } from './class_schedule.service';
 import { CreateClassScheduleDto } from './dto/create-class_schedule.dto';
 import { AuthGuard } from '@nestjs/passport';
 
-@Controller('class-schedule')
+@Controller('schedule')
 export class ClassScheduleController {
   constructor(private readonly classScheduleService: ClassScheduleService) {}
 
-  @Post()
+  @Post('learning')
   @UseGuards(AuthGuard('jwt'))
   async findAll(@Req() request: any, @Body() body: CreateClassScheduleDto) {
     const { id: userId } = request.user;
--- src/api/eating_schedule/eating_schedule.controller.ts
+++ src/api/eating_schedule/eating_schedule.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { EatingScheduleService } from './eating_schedule.service';
 import { CreateEatingScheduleDto } from './dto/create-eating_schedule.dto';
 
-@Controller('eating-schedule')
+@Controller('schedule')
 export class EatingScheduleController {
   constructor(private readonly eatingScheduleService: EatingScheduleService) {}
 
-  @Post()
+  @Post('eating')
   async findAll(@Body() body: CreateEatingScheduleDto) {
     const { startDate, endDate } = body;
     return this.eatingScheduleService.findByDateRange(startDate, endDate);
