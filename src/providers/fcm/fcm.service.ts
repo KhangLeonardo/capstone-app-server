1f6cd8f25aa867c375c071b8b4f9b506a77483c2
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-09-09 11:10:36 +0700

    Update fcm service

--- src/providers/fcm/fcm.service.ts
+++ src/providers/fcm/fcm.service.ts
@@ -9,7 +9,6 @@ export class FcmService {
   private readonly messaging: admin.messaging.Messaging;
 
   constructor() {
-    console.log('FIREBASE_PRIVATE_KEY:', process.env.FIREBASE_PRIVATE_KEY);
     const firebaseConfig = {
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
